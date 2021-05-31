56aef52eedaee10eccb7da01920bb4f5bddf3476
ThatMatrix <noe.urso1212@gmail.com>
2021-05-31 10:38:17 +0200

    Spawn works in storage room

---
+++ Assets/Scripts/Environnement/MobSpawning.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using Photon.Pun;
+using UnityEngine;
+using Random = System.Random;
+
+public class MobSpawning : MonoBehaviour
+{
+    public GameObject[] mobSpawnPoints;
+    public string[] nameMobsToSpawn;
+    private bool _hasBeenEnabled;
+
+    private void Start()
+    {
+        _hasBeenEnabled = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && nameMobsToSpawn.Length > 0 && !_hasBeenEnabled)
+        {
+            _hasBeenEnabled = true;
+            foreach (var spawnPoint in mobSpawnPoints)
+            {
+                int indexMobToSpawn = new Random().Next(nameMobsToSpawn.Length);
+                PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", nameMobsToSpawn[indexMobToSpawn]),
+                    spawnPoint.transform.position, spawnPoint.transform.rotation);
+            }
+        }
+    }
+}
