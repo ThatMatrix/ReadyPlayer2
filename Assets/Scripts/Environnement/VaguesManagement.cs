4408f4f7859ae550a0897dd8bc6c113431d721af
ThatMatrix <noe.urso1212@gmail.com>
2021-06-26 12:22:10 +0200

    mob spawn in waves probably

--- Assets/Scripts/Environnement/VaguesManagement.cs
+++ Assets/Scripts/Environnement/VaguesManagement.cs
@@ -1,14 +1,127 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using Photon.Pun;
 using UnityEngine;
 
 public class VaguesManagement : MonoBehaviour
 {
-    public int curVagues;
-    public int nbVaguesMax;
+    public int curWave;
+    public int maxWave;
+    public bool canSpawn;
 
-    bool HasEnemiesLeftOnTheMap()
+    public int[] nbMobWave;
+    public GameObject[] spawnPoints;
+    public string[] nameMobsToSpawn;
+
+    public GameObject Boss;
+
+    private bool HasEnemiesLeftOnTheMap()
     {
-        return GameObject.FindGameObjectsWithTag("Enemy")
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        foreach (var enemy in enemies)
+        {
+            if (enemy.activeSelf)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    void DeleteAllEnemies()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        foreach (var enemy in enemies)
+        {
+            PhotonView photonView = enemy.GetPhotonView();
+            
+            if (PhotonNetwork.IsMasterClient || photonView.AmOwner || photonView.IsMine)
+            {
+                Debug.Log("destroyed enemy");
+                PhotonNetwork.Destroy(gameObject);
+            }
+        }
+    }
+
+    bool HaveToDelete()
+    {
+        if (!canSpawn)
+        {
+            return false;
+        }
+        
+        if (curWave > maxWave)
+        {
+            Debug.Log("The current Wave is superior to the number of maximum wave : not spawning/deleting enemies");
+        }
+
+        return curWave < maxWave;
+    }
+
+    private void Start()
+    {
+        Boss.SetActive(false);
+        
+        if (nbMobWave.Max() < spawnPoints.Length)
+        {
+            throw new ArgumentException
+                ("the number of spawnPoint is inferior to the number of mobs to spawn at some point");
+        }
+
+        if (nbMobWave.Length +  1 < maxWave)
+        {
+            throw new ArgumentException("The number of waves is superior to the number of mobs to spawn");
+        }
+        
+        if (canSpawn)
+        {
+            curWave = 0;
+            canSpawn = HaveToDelete();
+        }
+        else
+        {
+            Debug.Log("canSpawn is at false, not spawning enemies");
+        }
+    }
+
+    private void Update()
+    {
+        canSpawn = HaveToDelete();
+        if (canSpawn && !HasEnemiesLeftOnTheMap())
+        {
+            Debug.Log("No enemies left on the map, incrementing the wave");
+            curWave += 1;
+            
+            SpawnWave();
+        }
+    }
+
+    void SpawnWave()
+    {
+        if (curWave == maxWave)
+        {
+            SpawnBossWave();
+        }
+        else
+        {
+            int nbMobToSpawn = nbMobWave[curWave - 1];
+            
+            for (int i = 0; i < nbMobToSpawn; i++)
+            {
+                GameObject spawnPoint = spawnPoints[i];
+                int indexMobToSpawn = new System.Random().Next(nameMobsToSpawn.Length);
+                
+                PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", nameMobsToSpawn[indexMobToSpawn]),
+                    spawnPoint.transform.position, spawnPoint.transform.rotation);
+            }
+        }
+    }
+
+    void SpawnBossWave()
+    {
+        Boss.SetActive(true);
     }
 }
