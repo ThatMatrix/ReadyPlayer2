bbdbef9255e08c242d0d9633a36a0adeae7d4bf8
Esteban Dulaurans <esteban.petit-dulaurans@orange.fr>
2021-06-26 18:22:30 +0200

    Kill me pls

---
+++ Assets/NPCTalkingBubble.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCTalkingBubble : MonoBehaviour
+{
+    private float timeBeforeDespawn;
+    public GameObject text;
+    public float cooldown;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeBeforeDespawn = Time.time + cooldown;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > timeBeforeDespawn)
+        {
+            text.SetActive(false);
+            gameObject.SetActive(false);
+        }
+    }
+}
