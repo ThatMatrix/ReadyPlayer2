ea7352b86d6a44e16925160713e034376ed51bf5
ThatMatrix <noe.urso1212@gmail.com>
2021-06-26 11:29:15 +0200

    Sceptic eye has tag enemy

---
+++ Assets/Scripts/Environnement/VaguesManagement.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VaguesManagement : MonoBehaviour
+{
+    public int curVagues;
+    public int nbVaguesMax;
+
+    bool HasEnemiesLeftOnTheMap()
+    {
+        return GameObject.FindGameObjectsWithTag("Enemy")
+    }
+}
