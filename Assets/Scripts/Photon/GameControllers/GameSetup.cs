50038a2de8aa8912987657fb335744697499903e
Esteban Dulaurans <esteban.petit-dulaurans@orange.fr>
2021-06-27 15:09:41 +0200

    MenuTheme implemented

--- Assets/Scripts/Photon/GameControllers/GameSetup.cs
+++ Assets/Scripts/Photon/GameControllers/GameSetup.cs
@@ -27,6 +27,7 @@ public class GameSetup : MonoBehaviour
     private void Start()
     {
         LaunchEvent();
+        FindObjectOfType<AudioManager>().Stop("NPCTheme");
         FindObjectOfType<AudioManager>().GameSetupPlay();
     }
 
